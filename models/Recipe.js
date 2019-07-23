2abffd3051f9b7551e3882af1d21a04ebcbdd2b5
manuelsilv <manuel-silva@live.nl>
2019-07-23 17:38:08 +0200

    iteration 1 recipe schema

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const title = require('title');
+const cuisine = require('cuisine');
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String, unique:true, required: true},
+  level: { type: String, value: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+  ingredients: { type: Array},
+  cuisine: {type: String},
+  dishType: {type: String},
+  image: {type: String},
+  duration : {type: Number},
+  creator: {type: String},
+  created: {type: Date},
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
