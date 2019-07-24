0935de05952ab634bfc21d4c9780849d7549f129
manuelsilv <manuel-silva@live.nl>
2019-07-24 11:31:51 +0200

    iteration 2

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const title = require('title');
-const cuisine = require('cuisine');
+const today = new Date(); // better use ...Date.now
 const recipeSchema = new Schema({
   title: { type: String, unique:true, required: true},
   level: { type: String, value: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
   ingredients: { type: Array},
-  cuisine: {type: String},
+  cuisine: {type: String, required: true},
   dishType: {type: String},
   image: {type: String},
   duration : {type: Number},
   creator: {type: String},
-  created: {type: Date},
+  created: {type: Date, default: today } // Date.now also works and is a shorter notation.
 });
-
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+ Recipe.create(new Recipe);
+
+//  .then = () =>{
+//   console.log("new recipe in progress...")
+//   return Recipe.insertMany(Data);
+//  })
+//  .then = () =>{
+//    console.log("recipe renewal updating..")
+//   return.update(title: "Chicken Massala");
+//  })
 
